f9a40fcf68b538febc8ad28f631e7062b98d5e8c
Brandon Aiton <brandonaiton@icloud.com>
2017-09-25 13:34:00 -0400

    All functionallity is working just have to style the page

---
+++ src/Components/BaseLayout.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+import Header from "./Header"
+import Footer from "./Footer"
+
+export default class BaseLayout extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>
+          {this.props.children}
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
